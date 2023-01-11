7bcec6b9fb62b158e7fd627c902dc707f9787c5a
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-11 21:36:34 +0200

    add init_db script

---
+++ scripts/init_db.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+source .env
+
+if [ -z $DATABASE_URL ]; then
+  echo "DATABASE_URL must be set in .env file" >&2
+  exit 1
+fi
+
+read -r DB_USER DB_PASSWORD DB_HOST DB_PORT DB_NAME <<< $( \
+  echo $DATABASE_URL | \
+  sed -E 's/postgres:\/\/(.+):(.+)@(.+):(.+)\/(.+)/\1 \2 \3 \4 \5/' \
+)
+
+docker run \
+  -d \
+  --name "${DB_NAME}_db" \
+  -e POSTGRES_USER="$DB_USER" \
+  -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+  -e POSTGRES_DB="$DB_NAME" \
+  -p "$DB_PORT:5432" \
+  postgres \
+  -N 100 \
+  >/dev/null
+
+sleep 1
+until psql $DATABASE_URL -c '\q'; do
+  echo "Failed to connect to the database. Retrying..." >&2
+  sleep 1
+done
+
+echo "Database is running on $DB_HOST:$DB_PORT"
