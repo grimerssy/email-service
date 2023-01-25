edc04418feabf2f75989a890ea28672f835e7c9b
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-25 15:30:22 +0200

    remove transactions in migrations
    migrations are already wrapped in a transaction

--- migrations/20230119174720_make_status_not_null_in_subscriptions.up.sql
+++ migrations/20230119174720_make_status_not_null_in_subscriptions.up.sql
@@ -1,7 +1,5 @@
-begin;
-  update subscriptions
-    set status = 'confirmed'
-    where status is null;
+update subscriptions
+set status = 'confirmed'
+where status is null;
 
-  alter table subscriptions alter column status set not null;
-commit;
+alter table subscriptions alter column status set not null;
